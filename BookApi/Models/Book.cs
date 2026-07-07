9cb67b26da8113c7145fada96c0b76d35d3bd71f
Noel Rydström <ironnoel33@gmail.com>
2026-07-07 15:27:23 +0200

    First commit backend setup

---
+++ BookApi/Models/Book.cs
@@ -0,0 +1,12 @@
+namespace BookApi.Models;
+
+public class Book
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Author { get; set; } = string.Empty;
+
+    public DateTime PublishDate { get; set; }
+}
