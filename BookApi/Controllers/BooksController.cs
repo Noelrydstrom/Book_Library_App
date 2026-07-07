88cd1e50598dc268eb1809863273a4ab78dd3760
Noel Rydström <ironnoel33@gmail.com>
2026-07-07 17:27:25 +0200

    Complete Book Crud API

---
+++ BookApi/Controllers/BooksController.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookApi.Data;
+using BookApi.Models;
+
+namespace BookApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public BooksController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+
+    // GET ALL
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+    {
+        return await _context.Books.ToListAsync();
+    }
+
+
+    // GET ID
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Book>> GetBook(int id)
+    {
+        var book = await _context.Books.FindAsync(id);
+
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        return book;
+    }
+
+
+    // POST
+    [HttpPost]
+    public async Task<ActionResult<Book>> CreateBook(Book book)
+    {
+        _context.Books.Add(book);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(
+            nameof(GetBook),
+            new { id = book.Id },
+            book
+        );
+    }
+
+
+    // PUT
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateBook(int id, Book book)
+    {
+        if (id != book.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(book).State = EntityState.Modified;
+
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+
+    // DELETE
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBook(int id)
+    {
+        var book = await _context.Books.FindAsync(id);
+
+        if (book == null)
+        {
+            return NotFound();
+        }
+
+        _context.Books.Remove(book);
+
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
