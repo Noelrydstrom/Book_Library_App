752ecd3a16e291420636c00902552359e1c57690
Noel Rydström <ironnoel33@gmail.com>
2026-07-13 16:56:42 +0200

    Frontend Structure start commit

---
+++ book-library-client/src/app/components/add-quote/add-quote.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-quote',
+  imports: [],
+  templateUrl: './add-quote.component.html',
+  styleUrl: './add-quote.component.css'
+})
+export class AddQuoteComponent {
+
+}
---
+++ book-library-client/src/app/components/edit-quote/edit-quote.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-quote',
+  imports: [],
+  templateUrl: './edit-quote.component.html',
+  styleUrl: './edit-quote.component.css'
+})
+export class EditQuoteComponent {
+
+}
