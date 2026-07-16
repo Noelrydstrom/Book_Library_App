fe46f89e83986b84c6b47bde8dd344dd23405284
Noel Rydström <ironnoel33@gmail.com>
2026-07-16 14:41:19 +0200

    FIxade FontAwsome Ikoner samt la till DarkMode

--- book-library-client/src/app/app.component.ts
+++ book-library-client/src/app/app.component.ts
@@ -20,11 +20,35 @@ export class AppComponent {
   constructor(
     public authService: AuthService,
     private router: Router
-  ) {}
+  ){
+
+    const savedTheme = localStorage.getItem('darkMode');
+
+    if(savedTheme === 'true'){
+
+        this.darkMode = true;
+
+        document.body.classList.add('dark-mode');
+
+    }
+
+  }
 
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  darkMode = false;
+
+  toggleDarkMode() {
+
+    this.darkMode = !this.darkMode;
+
+    document.body.classList.toggle('dark-mode');
+
+    localStorage.setItem('darkMode', this.darkMode.toString());
+
   }
 
 }
