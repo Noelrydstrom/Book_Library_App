70d89ea39a805fb47a9cc64e2cd4127c9a678688
Noel Rydström <ironnoel33@gmail.com>
2026-07-24 14:21:53 +0200

    Register Added

--- book-library-client/src/app/components/login/login.component.ts
+++ book-library-client/src/app/components/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- book-library-client/src/app/components/register/register.component.ts
+++ book-library-client/src/app/components/register/register.component.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  username = '';
+  password = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  register() {
+
+    const user = {
+      username: this.username,
+      password: this.password
+    };
+
+    this.authService.register(user).subscribe({
+
+      next: () => {
+
+        alert("User created successfully!");
+
+        this.router.navigate(['/login']);
+
+      },
+
+      error: (err) => {
+
+        if (err.status === 400) {
+          alert("Username already exists.");
+        } else {
+          alert("Could not register user.");
+        }
+
+      }
+
+    });
+
+  }
 
 }
